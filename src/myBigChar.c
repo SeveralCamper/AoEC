4aa85a1d590ce5f3200f9c1b039769d02c569cd4
Several_notebook <mikha.alkhimovich@mail.ru>
2022-03-22 10:44:59 +0700

    develop: smoe fixes

--- src/myBigChar.c
+++ src/myBigChar.c
@@ -1,9 +1,36 @@
 #include "myBigChar.h"
 
-int bc_printA (char * str)
-{
+int bc_printA (char * str) {
 	printf("\E(0%s", str);
 	printf("\E(B");
+
+    return EXIT_SUCCESS;
+}
+
+int bc_box(int x1, int y1, int x2, int y2) {
+  mt_gotoXY(y1, x1); //upper
+  printf("\u250C");
+  for(int i = 0; i < x2-x1; i++)
+  {
+   bc_printA("\u2500");
+  }
+  bc_printA("\u2510");
+
+  for(int i = 0; i < y2-y1; i++) //leftright
+  {
+    mt_gotoXY(y1+i+1, x1);
+    bc_printA("\u2502");
+    mt_gotoXY(y1+i+1, x2+1);
+    bc_printA("\u2502");
+  }
+
+  mt_gotoXY(y2+1, x1); //lower
+  bc_printA("\u2514");
+  for(int i = 0; i < x2-x1; i++)
+  {
+    bc_printA("\u2500");
+  }
+  bc_printA("\u2518");
 }
 
 int main() {
--- src/myTerm.c
+++ src/myTerm.c
@@ -2,27 +2,32 @@
 
 int mt_clrsrc(void) {
     printf("\033c");
-    return NORMAL_VALUE_S;
+
+    return EXIT_SUCCESS;
 }
 
 int mt_gotoXY (int x, int y) {
     printf("\033[%d;%dH", x, y);
-    return NORMAL_VALUE_S;
+
+    return EXIT_SUCCESS;
 }
 
 int mt_setfgcolor (colors_t color) {
     printf("\e[%dm", color);
-    return NORMAL_VALUE_S;
+
+    return EXIT_SUCCESS;
 }
 
 int mt_setbgcolor (colors_back_t color_back) {
     printf("\e[%dm", color_back);
-    return NORMAL_VALUE_S;
+
+    return EXIT_SUCCESS;
 }
 
 int mt_getscreensize (int * rows, int * cols) {
     printf("getsize -s %d %d", *rows, *cols);
-    return NORMAL_VALUE_S;
+    
+    return EXIT_SUCCESS;
 }
 
 /* int main() {
