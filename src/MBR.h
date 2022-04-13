feeb9219f0c9ba978748c00d66fc6c1853e16830
Several_notebook <mikha.alkhimovich@mail.ru>
2022-04-13 17:16:02 +0700

    develop: finish MBR.h

--- src/MBR.h
+++ src/MBR.h
@@ -38,29 +38,29 @@ typedef struct PART {
 } tPART;
 
 int g_lba2chs(tLBA orig, tCHS *dest);
-int g_lba2large(tLBA, tLARGE *);
-int g_lba2idechs(tLBA, tIDECHS *);
-int g_chs2large(tCHS, tLARGE *);
-int g_chs2lba(tCHS, tLBA *);
-int g_chs2idechs(tIDECHS, tLBA *);
-int g_large2chs(tLARGE, tCHS *);
-int g_large2idechs(tLARGE, tIDECHS *);
-int g_large2lba(tLARGE, tLBA *);
-int g_idechs2chs(tIDECHS. tCHS *);
-int g_idechs2lagre(tIDECHS. tLARGE *);
-int g_idechs2lba(tIDECHS. tLBA *);
+int g_lba2large(tLBA orig, tLARGE *dest);
+int g_lba2idechs(tLBA orig, tIDECHS *dest);
+int g_chs2large(tCHS orig, tLARGE *dest);
+int g_chs2lba(tCHS orig, tLBA * dest);
+int g_chs2idechs(tIDECHS orig, tLBA *dest);
+int g_large2chs(tLARGE orig, tCHS *dest);
+int g_large2idechs(tLARGE orig, tIDECHS *dest);
+int g_large2lba(tLARGE orig, tLBA *dest);
+int g_idechs2chs(tIDECHS orig, tCHS *dest);
+int g_idechs2lagre(tIDECHS orig, tLARGE * dest);
+int g_idechs2lba(tIDECHS orig, tLBA * dest);
 
-int a_lba2chs(tCHS geometry, tLBA, tCHS *);
-int a_lba2large(tLARGE geometry, tLBA, tLARGE *);
-int a_lba2idechs(tIDECHS geometry, tLBA, tIDECHS *);
-int a_chs2lba(tCHS geometry, tCHS, tLBA *);
-int a_large2lba(tLARGE geometry, tLARGE, tLBA *);
-int a_idechs2lba(tIDECHS geometry, tIDECHS, tLBA *);
-int a_large2chs(tLARGE geometry1, tCHS geometry2, tLARGE, tCHS *);
-int a_large2idechs(tLARGE geometry1, tIDECHS geometry2, tLARGE, tIDECHS *);
-int a_chs2large(tCHS geometry1, tLARGE geometry2, tCHS, tLARGE *);
-int a_idechs2large(tIDECHS geometry1, tLARGE geometry2, tIDECHS, tLARGE *);
-int a_chs2idechs(tCHS geometry1, tIDECHS geometry2, tCHS, tIDECHS *);
-int a_idechs2chs(tIDECHS geometry1, tCHS geometry2, tIDECHS, tCHS *);
+int a_lba2chs(tCHS geometry, tLBA orig, tCHS *dest);
+int a_lba2large(tLARGE geometry, tLBA orig, tLARGE *dest);
+int a_lba2idechs(tIDECHS geometry, tLBA orig, tIDECHS *dest);
+int a_chs2lba(tCHS geometry, tCHS orig, tLBA * dest);
+int a_large2lba(tLARGE geometry, tLARGE orig, tLBA *dest);
+int a_idechs2lba(tIDECHS geometry, tIDECHS orig, tLBA *dest);
+int a_large2chs(tLARGE geometry1, tCHS geometry2, tLARGE orig, tCHS *dest);
+int a_large2idechs(tLARGE geometry1, tIDECHS geometry2, tLARGE orig, tIDECHS *dest);
+int a_chs2large(tCHS geometry1, tLARGE geometry2, tCHS orig, tLARGE *dest);
+int a_idechs2large(tIDECHS geometry1, tLARGE geometry2, tIDECHS orig, tLARGE *dest);
+int a_chs2idechs(tCHS geometry1, tIDECHS geometry2, tCHS orig, tIDECHS *dest);
+int a_idechs2chs(tIDECHS geometry1, tCHS geometry2, tIDECHS orig, tCHS *dest);
 
 #endif  // SRC_MBR_H_
